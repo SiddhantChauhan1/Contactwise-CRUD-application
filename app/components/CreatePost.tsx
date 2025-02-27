d41ebc13eccdddc930f46e8344788450ebbd2a07
SiddhantChauhan1 <siddhantchauhan003@gmail.com>
2025-02-27 16:23:13 +0530

    Commit 4

--- app/components/CreatePost.tsx
+++ app/components/CreatePost.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import PostForm from "./PostForm";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Post } from "../interface/post";
